ae2bfdcfe691cc999bacdc8fbd7c38b0a4ecc1cc
keithatan <tan230@purdue.edu>
2019-01-16 18:18:39 -0500

    Created PhotosViewController class

---
+++ PhotosViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotosViewController.swift
+//  TumblrViewer
+//
+//  Created by Keith Tan on 1/16/19.
+//  Copyright © 2019 keithatan. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TumblrViewer/PhotosViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PhotosViewController.swift
+//  TumblrViewer
+//
+//  Created by Keith Tan on 1/16/19.
+//  Copyright © 2019 keithatan. All rights reserved.
+//
+
+import Foundation
